aa5fdb14707ef42be58701854eda1a9376e14ace
Mariano De Simone - Qida <mariano.desimone@qida.es>
2025-12-04 14:34:28 +0100

    Link Invoice Fix

---
+++ src/links/order-invoice.ts
@@ -0,0 +1,12 @@
+import InvoiceModule from "../modules/invoice"
+import OrderModule from "@medusajs/medusa/order"
+import { defineLink } from "@medusajs/framework/utils"
+
+export default defineLink(
+  OrderModule.linkable.order,
+  {
+    linkable: InvoiceModule.linkable.invoice,
+    isList: false,
+  }
+)
+
